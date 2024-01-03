8d677f22c4b9beff4ab4fd0ea17040ca22b40516
Raul Muñoz <Raulmunozn@gmail.com>
2024-01-03 18:40:50 -0400

    Added claim rewards

--- js/search.js
+++ js/search.js
@@ -1,3 +1,56 @@
+
+let nowDate = new Date(); 
+let mindate = new Date(nowDate.getTime() + 30*60*1000);
+let timeExpire = mindate.getTime();
+let authToken = "";
+function send() {
+    userField = document.getElementById("username");
+    username = userField.value;
+    window.hive_keychain.requestSignBuffer(
+        username,
+     `{"username":"${username}","type":"login","app":"Holodex","expiry":${timeExpire}}`,
+      "Active",
+      function (response) {
+        hiveResponse=response;
+        if(response.success == true) {
+            $("#ClaimPanel").show();
+            userField.setAttribute('readonly', true); 
+            userField.classList.remove('neon');
+            document.getElementById("userprofile").src = `https://images.hive.blog/u/${username}/avatar/small`;
+          
+                $.get(`https://api.holozing.com/rewards/pending?user=${username}`, function(data, status){
+                  $("#staking").html(data.stakeZing)
+                  $("#delegation").html(data.delegation)
+                  $("#posh").html(data.stakePosh)
+                  $("#total").html(Number(parseFloat(data.stakeZing)+parseFloat(data.stakePosh)+parseFloat(data.delegation)).toFixed(3))
+
+                });
+                $.post(`https://api.holozing.com/login`,response,function(data, status){
+                authToken=data.jwt;
+                });
+        }
+      },
+    );
+  }
+
+  $("#claim").click(function(){
+    if(authToken != "") {
+        $.ajax({
+            url:`https://api.holozing.com/rewards/pending/`,
+            type: 'POST',
+            headers: {"Authorization": `Bearer ${authToken}`, 'X-Alt-Referer': 'https://holozing.com/' } ,
+            success: function(data)
+            {
+            $("#ClaimPanel").hide();
+            $("#successful").show();
+            $("#staking").html(0)
+            $("#delegation").html(0)
+            $("#posh").html(0)
+            $("#total").html(0)
+        }       
+      });
+    }
+});
 var animateClass = "animatered";
 const data = [
     {
@@ -42,19 +95,39 @@ function search(searchTerm) {
     return results;
 }
 
+$("#rewards").click(function(){
+    $(".ButtonPanel").show();
+    
+    $("#ClaimPanel").show();
+})
+function reset() {
+    $(".ButtonPanel").show();
+    $("#statsDescription").hide();
+    document.getElementById("picture").style.backgroundImage = `url('')`;
+    $("#search").val("");
+    $("#ClaimPanel").hide();
+    $("#successful").hide();
+}
+
 document.getElementById("search").addEventListener("input", () => {
     const searchTerm = document.getElementById("search").value;
+    $(".ButtonPanel").hide();
+    if (searchTerm.length == 0){
+        reset();
+    }
 
     if (searchTerm.length > 2) {
         const results = search(searchTerm);
-        document.getElementById("stats").innerHTML = "";
+        document.getElementById("statsDescription").innerHTML = "";
+        $("#statsDescription").show();
+        
         document.getElementById("picturebg").classList.remove(`${animateClass}`);
         for (const result of results) {
             const p = document.createElement("p");
             animateClass = `${result.color}`;
 
             p.innerHTML = `<strong>Name:</strong> ${result.name} ${result.content}`;
-            document.getElementById("stats").appendChild(p);
+            document.getElementById("statsDescription").appendChild(p);
             document.getElementById("picture").style.backgroundImage = `url('${result.url}')`;
             document.getElementById("picturebg").classList.add(`${result.color}`);
         }
